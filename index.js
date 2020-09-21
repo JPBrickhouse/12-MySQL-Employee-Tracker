9151ceef43e8d0b1fb94e9f17ac34ff7c24e9fb7
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-21 17:29:48 -0500

    Switch cases built for the main menu

---
+++ index.js
@@ -0,0 +1,94 @@
+
+// Importing the necessary packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var cTable = require('console.table');
+
+// Creating a connection to the MySQL database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "", // INPUT PASSWORD
+    database: "" // INPUT DATABASE
+});
+
+// Making the connection to the database
+connection.connect(function (err) {
+    // Throwing an error if one occurs
+    if (err) throw err;
+    
+    // Running the employeManager function
+    employeeManager();
+});
+
+// The employeeManager function is the "main menu" for the employee manager application
+function employeeManager() {
+
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all employees by department",
+            "View all employees by role",
+            "Add department",
+            "Add role",
+            "Add employee",
+            "Update employee role",
+            "Exit"
+
+            // Above are the basic choices.
+            // If these are accomplished and there is time,
+            // consider adding more choices below:
+            // "Update employee manager"
+            // "View all employees by manager"
+            // "Delete department"
+            // "Delete role"
+            // "Delete employee"
+            // "View department budget"
+        ]
+    }).then(function (answer) {
+        switch (answer.action) {
+            case "View all employees": {
+
+                break;
+            }
+            case "View all employees by department": {
+
+                break;
+            }
+            case "View all employees by role": {
+
+                break;
+            }
+            case "Add department": {
+
+                break;
+            }
+            case "Add role": {
+
+                break;
+            }
+            case "Add employee": {
+
+                break;
+            }
+            case "Update employee role": {
+
+                break;
+            }
+            case "Exit": {
+                connection.end();
+                break;
+            }
+        }
+    })
+}
